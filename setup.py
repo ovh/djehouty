d4a0c7a6ab27ad671e52718ea838dd7d230ca036
Pierre De Paepe <pierre.de-paepe@corp.ovh.com>
2016-01-23 13:11:43 +0100

    be clear

--- setup.py
+++ setup.py
@@ -28,37 +28,39 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """
-Djehouty is intended to be a set of logging formatters and handlers to easily send log entries.
+Djehouty is intended to be a set of logging formatters and handlers to easily
+send log entries.
 """
 
 from setuptools import setup, find_packages
 
-version = '0.1.0'
+VERSION = '0.1.0'
 
 setup(
-    name                = 'djehouty',
-    version             = version,
-    description         = "Djehouty is intended to be a set of logging formatters and handlers to easly send log entries.",
-    long_description    = """""",
-    classifiers         = [
+    name='djehouty',
+    version=VERSION,
+    description="Djehouty is intended to be a set of logging formatters and " +
+		"handlers to easily send log entries.",
+    long_description="""""",
+    classifiers=[
         "License :: OSI Approved :: BSD License",
         "Operating System :: POSIX",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
     ],
-    keywords            = 'GELF, LTSV',
-    author              = 'Cedric DUMAY',
-    author_email        = 'cedric.dumay@gmail.com',
-    url                 = 'https://github.com/cdumay/djehouty',
-    license             = 'BSD',
-    packages            = find_packages('src'),
-    package_dir         = {'': 'src'},
-    include_package_data= True,
-    zip_safe            = True,
-    install_requires    = (
+    keywords='GELF, LTSV',
+    author='Cedric DUMAY',
+    author_email='cedric.dumay@corp.ovh.com',
+    url='https://github.com/ovh/djehouty',
+    license='BSD',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=(
         'distribute',
     ),
-    entry_points        = """
+    entry_points="""
 """,
 )
 
